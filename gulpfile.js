ac9842f94130c64ce21063b583b8583a680fd9ed
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-05-05 15:09:22 +0200

    add(core) implement gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,39 @@ var jscs = require('gulp-jscs');
 var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
 var mocha  = require('gulp-mocha');
+var _  = require('lodash');
+var path = require('path');
 
 var paths = {
-  lint: ['./gulpfile.js', './app/index.js'],
-  tests: ['./test/**/*.js', '!test/{temp,temp/**}'],
-  source: ['./app/index.js']
+  lint: ['gulpfile.js', 'app/index.js'],
+  tests: ['test/**/*.js', '!test/temp/**'],
+  source: ['app/index.js']
 };
+
+function joinPath(source) {
+  if (_.isArray(source)) {
+    var temp = [];
+    _.each(source, function (item) {
+      temp.push(joinPath(item));
+    });
+    return temp;
+  } else {
+    if (source.indexOf('!') === 0) {
+      return '!' + path.join(__dirname, source.slice(1));
+    } else {
+      return path.join(__dirname, source);
+    }
+  }
+}
+
+_.each(paths, function (sources, key, list) {
+  list[key] = joinPath(sources);
+});
 
 gulp.task('lint', function () {
   return gulp.src(paths.lint)
-    .pipe(jshint('.jshintrc'))
-    .pipe(jscs())
+    .pipe(jshint(path.join(__dirname, '.jshintrc')))
+    .pipe(jscs(path.join(__dirname, '.jscs.json')))
     .pipe(jshint.reporter(stylish));
 });
 
@@ -32,3 +54,8 @@ gulp.task('istanbul', function (cb) {
 });
 
 gulp.task('test', ['lint', 'istanbul']);
+
+gulp.task('watch', function () {
+  gulp.run('istanbul');
+  gulp.watch(_.union(paths.lint, paths.tests), ['istanbul']);
+});
